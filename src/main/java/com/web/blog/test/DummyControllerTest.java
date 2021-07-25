78687025e01a1ca6aba71a50d9f165867132ab47
gisung2215 <wjv2215@naver.com>
2021-07-25 16:57:45 +0900

    feat: JPA update 더티 체킹!

--- src/main/java/com/web/blog/test/DummyControllerTest.java
+++ src/main/java/com/web/blog/test/DummyControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -39,7 +37,6 @@ public class DummyControllerTest {
                 .orElseThrow(() -> new IllegalArgumentException("해당 id의 유저는 없습니다."));
     }
 
-
     @GetMapping("/dummy/user/list")
     public List<User> list(){
         return userRepository.findAll();
@@ -52,5 +49,28 @@ public class DummyControllerTest {
         Page<User> users = userRepository.findAll(pageable);
         return users;
     }
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User update(@PathVariable Long id, @RequestBody User userReq){
+
+        // json 데이터 요청 =>
+        //         MessageConverter의 jackson 라이브러리가 받아준다.
+        User findUser = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("요청하신 id는 존재하지 않습니다."));
+
+        findUser.setEmail(userReq.getEmail());
+        findUser.setPassword(userReq.getPassword());
+//        userRepository.save(findUser);
+//        save함수는 id를 전달하지 않으면 insert를 해주고
+//        save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+//        save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+//        @Transactional 을 달면 save를 하지 않아도 update가 된다!!!(더티 체킹)
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 id는 존재하지 않습니다."));
+
+    }
+
 
 }
